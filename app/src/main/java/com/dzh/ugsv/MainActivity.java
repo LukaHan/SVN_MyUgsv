c4424a28d1c4825d19af6fd3d67432193d0e06ff
LukaHan <397308937@qq.com>
2018-07-16 14:14:57 +0800

    add baseapplication

--- app/src/main/java/com/dzh/ugsv/MainActivity.java
+++ app/src/main/java/com/dzh/ugsv/MainActivity.java
@@ -1,11 +1,11 @@
 package com.dzh.ugsv;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-import com.tencent.qcloud.xiaoshipin.mainui.TCSplashActivity;
+import com.tencent.qcloud.xiaoshipin.mainui.TCMainActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tvGoUgsv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, TCSplashActivity.class));
+                startActivity(new Intent(MainActivity.this, TCMainActivity.class));
             }
         });
     }

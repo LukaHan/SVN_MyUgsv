58968626fbf89a5da27f4ba83266ba40d53a66f6
LukaHan <397308937@qq.com>
2018-11-08 11:11:30 +0800

    新增视频发布页

---
+++ ugsv/src/main/java/com/tencent/qcloud/xiaoshipin/videopublish/TCVideoPublishActivity.java
@@ -0,0 +1,16 @@
+package com.tencent.qcloud.xiaoshipin.videopublish;
+
+import android.os.Bundle;
+
+import com.tencent.qcloud.xiaoshipin.R;
+import com.tencent.qcloud.xiaoshipin.common.activity.TCBaseActivity;
+
+public class TCVideoPublishActivity extends TCBaseActivity {
+    public TCVideoPublishActivity() {
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.activity_video_publish);
+    }
+}

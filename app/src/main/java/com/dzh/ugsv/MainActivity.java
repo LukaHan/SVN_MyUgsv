725119356b4c2e84ebaafbf0291b473698917576
LukaHan <397308937@qq.com>
2018-11-15 17:01:42 +0800

    删除无用的300+文件

--- app/src/main/java/com/dzh/ugsv/MainActivity.java
+++ app/src/main/java/com/dzh/ugsv/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.tencent.qcloud.xiaoshipin.Ugsv;
 import com.tencent.qcloud.xiaoshipin.mainui.TCMainActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         findViewById(R.id.tvGoUgsv).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Ugsv.init(getApplication());
                 startActivity(new Intent(MainActivity.this, TCMainActivity.class));
             }
         });
